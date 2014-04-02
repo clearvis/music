a1a3fac1cef75671692a879e502a1789e97b2ccb
James Sacci <james.sacci@prowebinnovations.com>
2014-04-02 14:09:29 -0400

    made top-bar container full width

--- header.php
+++ header.php
@@ -43,17 +43,22 @@
 			<div id="container">
 
 				<header class="header" role="banner">
+                
+                	<div class="navbar-container">
+                    	<div class="row">
+							<?php get_template_part( 'partials/nav', 'offcanvas' ); ?>
+    
+                             <?php // get_template_part( 'partials/nav', 'topbar' ); ?>
+    
+                             <?php // get_template_part( 'partials/nav', 'offcanvas-sidebar' ); ?>
+    
+                            <!-- You only need to use one of the above navigations.
+                                 Offcanvas-sidebar adds a sidebar to a "right" offcanavas menus. -->
+                             
+                        </div>
+                    </div>
 
 					<div id="inner-header" class="row">
-                    
-                    	<?php get_template_part( 'partials/nav', 'offcanvas' ); ?>
-
-						 <?php // get_template_part( 'partials/nav', 'topbar' ); ?>
-
-						 <?php // get_template_part( 'partials/nav', 'offcanvas-sidebar' ); ?>
-
-						<!-- You only need to use one of the above navigations.
-							 Offcanvas-sidebar adds a sidebar to a "right" offcanavas menus. -->
                     
 						<div class="large-12 columns">
 							<h1>
