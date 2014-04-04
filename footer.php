7454bdf53f3fefbc99347b8fc2db96521886f231
James Sacci <james.sacci@prowebinnovations.com>
2014-04-04 15:27:20 -0400

    added address to footer

--- footer.php
+++ footer.php
@@ -8,9 +8,22 @@
 		    					</nav>
 		    				</div>
 			               
-			                <div class="large-12 medium-12 columns">		
+			                <div class="large-6 medium-6 columns">		
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+                                <p><a href="#">Privacy Policy</a></p>
 							</div>		
+                            
+                            <div class="large-6 medium-6 columns">	
+                            
+                            	<address class="text-left right">	
+                                  <span>The Mill at Lebanon</span>
+                                  <span>300 N. Maple Street, C/3</span>
+                                  <span>Lebanon, TN 37087</span>
+                                  <span>615-445-0612</span>
+                                </address>
+
+							</div>
+                            
 						</div> <!-- end #inner-footer -->			
 					</footer> <!-- end .footer -->
 				</div> <!-- end #container -->
